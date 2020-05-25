85f02f8c46052dfa2af64657aaf991713cd6c74d
maximkryukov <kryukov.ma@phystech.edu>
2020-05-26 00:21:54 +0200

    added some methods in object detection and robot tracking

--- project/object_detection.py
+++ project/object_detection.py
@@ -1,42 +1,87 @@
-import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-from skimage.filters import gaussian
-from skimage.morphology import binary_erosion, binary_dilation, binary_opening, binary_closing, disk, square
+from skimage.filters import threshold_otsu
+from skimage.morphology import binary_dilation, binary_closing, disk, square
 from skimage.feature import canny
 from skimage.color import rgb2gray
 
 from region_growing import region_growing
 
 
-def extract_objects(image, objects_shape=(28, 28)):
-    # Extract edges
+def extract_objects(image, method='otsu'):
+    """
+        Get extracted objects (operators and digits) from the image. 
+        You can use different methods to do that. 'canny' method which uses 
+        Canny filter for edge detection. 'manual_threshold' applies manual threshold
+        of 128 to grayscaled image, while 'otsu' applies Otsu threshold (by default).
+        
+        Also during shapes extraction from mask you can use either binary_closing 
+        (by default) or binary_dilation.
+        
+        Parameters
+        ----------
+        image : 2D array of RGB image
+            The frame image
+        method : str
+            Could be either 'canny', 'manual_threshold' or 'otsu'
+        objects_shape : tuple of int
+            The shape of output object images
+            
+        Returns
+        -------
+        boxes_reduced : list
+            The list of object (operators and digits) boxes.
+    """
+    
+    # Get grayscaled image
     grayscale = rgb2gray(image) * 255
-    edges = canny(grayscale / 255.)  # We will use popular Canny filter for edge detection
+    
+    if method == 'canny':
+        object_regions = canny(grayscale / 255.)  # We will use popular Canny filter for edge detection
+    elif method == 'otsu':
+        thresh = threshold_otsu(grayscale)
+        object_regions = grayscale < thresh
+    elif method == 'manual_threshold':
+        thresh = 128
+        object_regions = grayscale < thresh
+    else:
+        NotImplementedError
 
-    # Fill object regions
-    square_size = [2, 5, 10]
-    mask_similarity_threshold = 0.999
+    fill_by = 'binary_closing'  # or 'binary_dilation'
+    
+    if fill_by == 'binary_closing':
+        # Fill object regions
+        square_size = [2, 5, 10]
+        mask_similarity_threshold = 0.999
 
-    shapes_mask = binary_closing(edges, selem=disk(square_size[0]))
-    for size in square_size[1:]:
-        new_shapes_mask = binary_closing(edges, selem=disk(size))
-        if np.sum(new_shapes_mask == shapes_mask) / shapes_mask.size > mask_similarity_threshold:
-            print(f"Chose size {size}")
-            break
-        shapes_mask = new_shapes_mask
-
-    # plt.figure(figsize=(10, 10))
-    # plt.imshow(shapes_mask, cmap="gray")
-    # plt.show()
+        shapes_mask = binary_closing(object_regions, selem=disk(square_size[0]))
+        for size in square_size[1:]:
+            new_shapes_mask = binary_closing(object_regions, selem=disk(size))
+            if np.sum(new_shapes_mask == shapes_mask) / shapes_mask.size > mask_similarity_threshold:
+                print(f"Chose size {size}")
+                break
+            shapes_mask = new_shapes_mask
+    # This method produces bigger boxes
+    elif fill_by == 'binary_dilation':
+        # Manual threshold for dilation using square element
+        square_size = 7
+        shapes_mask = binary_dilation(object_regions, selem=square(7))
 
     # Get object regions
     regions = region_growing(shapes_mask)
     regions = [np.array(region) for region in regions]
-
+    
+    # Get rid off the arrow - biggest region
+    len_arrow_region = len(max(regions, key=len))
+    regions = [region for region in regions if len(region) < len_arrow_region]
+    
+    if fill_by == 'binary_closing':
+        # Adding pads on the edges
+        pad = 2
+    elif fill_by == 'binary_dilation':
+        # Dilation already adds padding
+        pad = 0
+     
     # Get bounding boxes of objects
-    pad = 2
     boxes = [(max(min(region[:, 1]) - pad, 0),
               max(min(region[:, 0]) - pad, 0),
               min(max(region[:, 1]) + pad, image.shape[1]),
@@ -46,19 +91,13 @@ def extract_objects(image, objects_shape=(28, 28)):
     # Delete false object detections
     boxes_reduced = []
     threshold_ratio = 5
+    min_height_threshold = 10
+    max_height_threshold = 40
 
     for box in boxes:
         width = box[2] - box[0]
         height = box[3] - box[1]
-        if width / height < threshold_ratio and height / width < threshold_ratio:
+        if (width / height < threshold_ratio) and (height / width < threshold_ratio) and (40 > height > 10) and (40 > width > 10):
             boxes_reduced.append(box)
 
     return boxes_reduced
-
-    # # Extract objects from image
-    # objects = [image[box[1]:box[3], box[0]:box[2]] for box in boxes_reduced]
-    #
-    # # Make objects' images to have the same size
-    # objects = [resize(obj, objects_shape) for obj in objects]
-    #
-    # return objects, boxes_reduced
